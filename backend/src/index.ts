3ec104768fb8b2f2e777aae38249204045dfa6ba
Lucas Leonel <lucleonelg4@gmail.com>
2026-08-30 21:03:24 -0300

    arrumando package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,25 @@
 import fastify from "fastify"; 
 import cors from "@fastify/cors";
 import autoload from "@fastify/autoload";
+import path from "node:path";
+import log from "consola";
+import { process } from "zod/v4/core";
 
-const app = fastify();'
-app.register(cors, {origin })
+const app = fastify();
+app.register(cors, {origin: "*"});
+app.register(autoload, {
+    dir:path.join(import.meta.dirname, "routes"),
+    routeParams: true
+});
+
+app.addHook("onRoute",({ method , path }) => {
+    if (method === "HEAD" || method === "OPTIONS") return; 
+    log.success(`${method} ${path}`)
+});
+
+const port = 3000; 
+
+await app.listen({port, host: "0.0.0.0"}).catch(err => {
+    log.error(err);
+})
+log.success(`Server listening on ${port}`); 
